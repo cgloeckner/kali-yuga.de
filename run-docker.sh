fa2ed4876404bf0d454696b0d967e828d5de8a01
cgloeckner <cgloeckner@freenet.de>
2023-05-30 10:02:33 +0200

    rerun docker stops last instance

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-docker run -d --restart on-failure -p 8001:80 kali-page:latest
+FILE="kalipage.id"
+
+if test -f "$FILE"; then
+        # start old instance
+        OLD_ID=`cat $FILE`
+	echo "Stopping docker #$OLD_ID"
+        docker stop $OLD_ID
+        docker rm $OLD_ID
+fi
+
+# start new instance
+DOCKER_ID=`docker run -d --restart always -p 8002:80 kali-page:latest`
+echo "Started docker $DOCKER_ID"
+echo $DOCKER_ID > $FILE
